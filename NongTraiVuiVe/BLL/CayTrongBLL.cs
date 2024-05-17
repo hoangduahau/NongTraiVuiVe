247c00345811b3c786129a55a3735a9eb4e56c27
hoangduahau <aurelion293@gmail.com>
2024-05-18 00:31:56 +0700

    Fix base delete

--- NongTraiVuiVe/BLL/CayTrongBLL.cs
+++ NongTraiVuiVe/BLL/CayTrongBLL.cs
@@ -84,10 +84,6 @@ namespace NongTraiVuiVe.BLL
         public bool XoaCayTrong(int maCayTrong)
         {
 
-            // Kiểm tra MaCayTrong có giá trị hay không
-            if (cayTrong.MaCayTrong == 0)
-                throw new ArgumentException("Mã cây trồng không được để trống.");
-
             return _cayTrongDAL.XoaCayTrong(maCayTrong); // Gọi phương thức xóa của DAL
         }
 
